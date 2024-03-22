9c6ec8eafca822b2cdc791d1cd08863683dd36ae
kevingustianthe <gustiankevin@gmail.com>
2024-03-22 21:25:07 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,20 +3,12 @@ package main
 import (
 	"MyGram/database"
 	"MyGram/router"
-	"log"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(".env is not loaded properly")
-	}
-
 	database.StartDB()
 	r := router.StartApp()
-	err = r.Run()
+	err := r.Run()
 	if err != nil {
 		return
 	}
